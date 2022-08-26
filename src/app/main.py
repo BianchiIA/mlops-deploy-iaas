21712136fe5fc95d1d619b3bb32db230db9686f1
Vinicius Bianchi Soares <vinibianch@outlook.com>
2022-08-26 18:09:13 -0300

    atualizando server2

--- src/app/main.py
+++ src/app/main.py
@@ -77,4 +77,5 @@ def cotacao2():
 
 if __name__ == '__main__':
     from waitress import serve
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    serve(app, host="0.0.0.0", port=8080)
+    #app.run(debug=False, host='0.0.0.0', port=5000)
